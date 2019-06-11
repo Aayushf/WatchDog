0321241a29ffd4747e44bdf030c775102b2f7f31
Aayushf <aayushf@gmail.com>
2019-06-11 15:02:07 +0530

    Parse Response String, Display WiFi networks in Recycler, and update url to get wifi nets from pi.

--- app/src/main/java/com/aayushf/watchdog/MainActivity.kt
+++ app/src/main/java/com/aayushf/watchdog/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.aayushf.watchdog
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -20,11 +29,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         val queue = Volley.newRequestQueue(this@MainActivity)
-        val url = "http://192.168.4.1:9890/"
+        val url = "http://192.168.4.1:9890/wifi"
+        main_rv.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             Response.Listener<String> { response ->
-                main_text.text = "Response is: ${response}"
+                var resp = response.substringAfter('\'')
+                resp = resp.substringBeforeLast('\'')
+                main_text.text = "Response is: ${resp}"
+                val list = resp.split("', '")
+                Log.d("MainActivity", list[0])
+                val ia = ItemAdapter<WiFiItem>()
+                val fa = FastAdapter.with(ia)
+                main_rv.adapter = fa
+                var items = mutableListOf<WiFiItem>()
+                list.forEach {
+                    items.add(WiFiItem(it))
+                }
+                ia.add(items)
+
             },
             Response.ErrorListener { main_text.text = "That didn't work!" })
         fab.setOnClickListener { view ->
@@ -47,6 +70,24 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+    class WiFiItem(var ssid:String = "Not Put"): AbstractItem<WiFiItem.ViewHolder>() {
+        override val layoutRes: Int
+            get() = R.layout.main_rv_card
+        override val type: Int
+            get() = 0
+        override fun getViewHolder(v: View): WiFiItem.ViewHolder {
+            return ViewHolder(v)
+        }
+        class ViewHolder(var iv: View) : FastAdapter.ViewHolder<WiFiItem>(iv) {
+            override fun bindView(item: WiFiItem, payloads: MutableList<Any>) {
+                iv.findViewById<TextView>(R.id.main_rv_card_text).text = item.ssid
+            }
+
+            override fun unbindView(item: WiFiItem) {
+
+            }
         }
     }
 }
