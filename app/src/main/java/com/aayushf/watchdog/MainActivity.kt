fd26bc500be99e71f76135cc3a07d3acde9e14b3
Aayushf <aayushf@gmail.com>
2019-06-12 12:02:56 +0530

    Demamn Password on Clicking wifi network, and send request to pi to save it.

--- app/src/main/java/com/aayushf/watchdog/MainActivity.kt
+++ app/src/main/java/com/aayushf/watchdog/MainActivity.kt
@@ -2,35 +2,37 @@ package com.aayushf.watchdog
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
 import com.mikepenz.fastadapter.items.AbstractItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var queue: RequestQueue? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        val queue = Volley.newRequestQueue(this@MainActivity)
-        val url = "http://192.168.4.1:9890/wifi"
+        val url = "http://192.168.4.1:9890/wifi/"
         main_rv.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+
+        queue = Volley.newRequestQueue(this@MainActivity)
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             Response.Listener<String> { response ->
@@ -47,13 +49,22 @@ class MainActivity : AppCompatActivity() {
                     items.add(WiFiItem(it))
                 }
                 ia.add(items)
+                fa.onClickListener = { view, adapter, item, position ->
+                    var ssid = item.ssid
+                    MaterialDialog(this).show {
+                        input { materialDialog, charSequence ->
+                            connectToWifi(ssid, charSequence.toString())
+                        }
+                    }
+                    true
+                }
 
             },
             Response.ErrorListener { main_text.text = "That didn't work!" })
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-            queue.add(stringRequest)
+            queue!!.add(stringRequest)
         }
     }
 
@@ -72,14 +83,23 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-    class WiFiItem(var ssid:String = "Not Put"): AbstractItem<WiFiItem.ViewHolder>() {
+    fun connectToWifi(ssid:String, passwd:String){
+        var newurl = "http://192.168.4.1:9890/wifi/connect/?ssid=${ssid}&passwd=${passwd}"
+        val srq = StringRequest(Request.Method.GET, newurl, Response.Listener<String> {  }, Response.ErrorListener {  })
+        queue!!.add(srq)
+
+    }
+
+    class WiFiItem(var ssid: String = "Not Put") : AbstractItem<WiFiItem.ViewHolder>() {
         override val layoutRes: Int
             get() = R.layout.main_rv_card
         override val type: Int
             get() = 0
+
         override fun getViewHolder(v: View): WiFiItem.ViewHolder {
             return ViewHolder(v)
         }
+
         class ViewHolder(var iv: View) : FastAdapter.ViewHolder<WiFiItem>(iv) {
             override fun bindView(item: WiFiItem, payloads: MutableList<Any>) {
                 iv.findViewById<TextView>(R.id.main_rv_card_text).text = item.ssid
