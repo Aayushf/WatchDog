6f36bd9f6d84342deccdab63c89f34d5ff4d3bfa
Aayushf <aayushf@gmail.com>
2019-06-26 15:06:02 +0530

    Added Log Display Functionality to App, it can now display images with filenames, as taken by the rpi.

--- app/src/main/java/com/aayushf/watchdog/HomeActivity.kt
+++ app/src/main/java/com/aayushf/watchdog/HomeActivity.kt
@@ -1,17 +1,33 @@
 package com.aayushf.watchdog
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.ActionBarDrawerToggle
-import android.view.MenuItem
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
+import kotlinx.android.synthetic.main.content_scrolling.*
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -19,8 +35,39 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        val db = FirebaseDatabase.getInstance()
+        val logs_ref = db.getReference("logs").child("log_images")
+        val logs_ref_cloud = FirebaseStorage.getInstance().getReference().child("logs")
+        val logitems = mutableListOf<LogItem>()
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                dataSnapshot.children.forEach {
+                    val fname = it.key!! + ".jpeg"
+                    logs_ref_cloud.child(fname).downloadUrl.addOnSuccessListener {uri:Uri->
+                        logitems.add(LogItem(uri.toString(), fname))
+                        Log.d("HomeActivity", "Getting URL succeeded$uri")
+                        val itemAdapter = ItemAdapter<LogItem>()
+                        val fastAdapter = FastAdapter.with(itemAdapter)
+                        Log.d("HomeActivity", "SETTINGADAPTER")
+                        home_rv.layoutManager = LinearLayoutManager(this@HomeActivity, RecyclerView.VERTICAL, false)
+                        home_rv.adapter = fastAdapter
+                        Log.d("HOMEACTIVITY", logitems.size.toString())
+                        itemAdapter.add(logitems)
+                        home_rv.adapter = fastAdapter
+                    }.addOnFailureListener {
+                        Log.d("HomeActivity", "Getting URL failed")
+                    }
+                    Log.d("HomeActivity", it.key!!)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+        }
+        logs_ref.addValueEventListener(postListener)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
@@ -34,7 +81,6 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         navView.setNavigationItemSelectedListener(this)
     }
 
@@ -89,5 +135,33 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    class LogItem(val url: String = "URL_NOT_GIVEN", val name: String = "NAME_NOT_GIVEN") :
+        AbstractItem<LogItem.ViewHolder>() {
+        init {
+            Log.d("HomeActivity", "LogItem Made")
+        }
+        override val layoutRes: Int
+            get() = R.layout.home_rv_card
+        override val type: Int
+            get() = 0
+
+        override fun getViewHolder(v: View): ViewHolder {
+            return ViewHolder(v)
+        }
+
+        class ViewHolder(val v: View) : FastAdapter.ViewHolder<LogItem>(v) {
+            override fun bindView(item: LogItem, payloads: MutableList<Any>) {
+                val iv = v.findViewById<ImageView>(R.id.rv_item_image)
+                Glide.with(iv).load(item.url).into(iv)
+                v.findViewById<TextView>(R.id.rv_item_text).text = item.name
+            }
+
+            override fun unbindView(item: LogItem) {
+
+            }
+
+        }
     }
 }
